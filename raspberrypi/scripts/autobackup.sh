9ee660e6defef74c04d97a88c86ecdeebe8cb1bf
JonathanZ837 <jonathanzeng2006@gmail.com>
2026-09-17 13:59:55 -0400

    auto backup config and IP address

--- raspberrypi/scripts/autobackup.sh
+++ raspberrypi/scripts/autobackup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-cp //boot/firmware/config.txt ~/creative-embedded-systems/raspberrypi/.config 
-hostname -I > ~/creative-embedded-systems/raspberrypi/.config/ip.md
+cp //boot/firmware/config.txt //home/stu3340/creative-embedded-systems/raspberrypi/.config
+cp //etc/profile //home/stu3340/creative-embedded-systems/raspberrypi/.config 
+cp //etc/passwd //home/stu3340/creative-embedded-systems/raspberrypi/.config 
+cp //etc/ssh/sshd_config //home/stu3340/creative-embedded-systems/raspberrypi/.config 
+cp //etc/ssh/ssh_config //home/stu3340/creative-embedded-systems/raspberrypi/.config 
+cp //boot/autobackup.sh //home/stu3340/creative-embedded-systems/raspberrypi/scripts/autobackup.sh
+
+hostname -I > //home/stu3340/creative-embedded-systems/raspberrypi/.config/ip.md
 git -C //home/stu3340/creative-embedded-systems/raspberrypi/.config commit -am "auto backup config and IP address"
 git -C //home/stu3340/creative-embedded-systems/raspberrypi/.config push origin main

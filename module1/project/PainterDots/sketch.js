cfef5ea94a3b883de2e4c1e6058baaeecdc4bf05
JonathanZ837 <jonathanzeng2006@gmail.com>
2026-09-22 19:05:42 -0400

    refactored code + made pathfinding more straightforward

--- module1/project/PainterDots/sketch.js
+++ module1/project/PainterDots/sketch.js
@@ -13,8 +13,10 @@ let painterIdleTime = 5;
 let lerpSpeed = 0.05;
 let painterStrokeWeight = 8;
 let painterDotOpacity = 170;
-//let painterDirections = [[1, 1], [1, 0], [1, -1], [0, 1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
-let painterDirections = [[[-1, 0], [-1, 1], [0, 1]], [[-1, 0], [-1, 1], [0, 1], [-1, -1], [0, -1]], [[-1, 0], [-1, -1], [0, -1]], [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0]],[[-1, 0], [-1, 1], [-1, -1], [0, -1], [0,1], [1, 0], [1, -1], [1,1]], [[-1, 0], [-1, -1], [0, -1], [1, 0], [1, -1]],[[1, 0], [0, 1], [1, 1]],[[1, 0], [0, 1], [1, 1], [0, -1], [1, -1]], [[1, 0], [0, -1], [1, -1]]] // bottom left, bottom center, bottom right, middle left, middle center, middle right, upper left, upper center, upper right
+let painterDirections = [[1, 1], [1, 0], [1, -1], [0, 1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
+//let painterDirections = [[[-1, 0], [-1, 1], [0, 1]], [[-1, 0], [-1, 1], [0, 1], [-1, -1], [0, -1]], [[-1, 0], [-1, -1], [0, -1]], [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0]],[[-1, 0], [-1, 1], [-1, -1], [0, -1], [0,1], [1, 0], [1, -1], [1,1]], [[-1, 0], [-1, -1], [0, -1], [1, 0], [1, -1]],[[1, 0], [0, 1], [1, 1]],[[1, 0], [0, 1], [1, 1], [0, -1], [1, -1]], [[1, 0], [0, -1], [1, -1]]] // bottom left, bottom center, bottom right, middle left, middle center, middle right, upper left, upper center, upper right
+
+let painterStartingPositions = [[]]
 let colorPalette = [[1, 41, 95], [132, 147, 36], [255, 179, 15], [253, 21, 27]];
 
 class Dot {
@@ -45,75 +47,51 @@ class Painter {
 		this.state = 'IDLE'
 		this.idleTime = painterIdleTime;
 		this.movingLerp = 0;
+		this.lastDirection = [0,0]
 
 		startingDot.painter = this;
 	}
 
-	getDirectionType() {
-		let directionType = -1;
-		// determine direction based on boundary conditions :o				
-		if (this.currDot.i == 0) { // upper
-			if (this.currDot.j == 0) {
-				directionType = 6;
-			} else if (this.currDot.j == width - 1) {
-				directionType = 8;
-			} else {
-				directionType = 7;
-			}
-		} else if (this.currDot.i == height - 1) { // lower
-			if (this.currDot.j == 0) {
-				directionType = 0;
-			} else if (this.currDot.j == width - 1) {
-				directionType = 2;
-			} else {
-				directionType = 1;
-			}
-		} else {
-			if (this.currDot.j == 0) { // middle
-				directionType = 3;
-			} else if (this.currDot.j == width - 1) {
-				directionType = 5;
-			} else {
-				directionType = 4;
+	getDirection() {
+		let possibleIndices = []
+		for (let i = 0; i < painterDirections.length; i++) {
+			let possibleDirection = painterDirections[i]
+			if (this.currDot.i + possibleDirection[0] < height && this.currDot.i + possibleDirection[0] >= 0 && this.currDot.j + possibleDirection[1] < width && this.currDot.j + possibleDirection[1] >= 0) {
+				console.log(this.currDot.i + possibleDirection[0],this.currDot.j + possibleDirection[1] )
+				let nextDot = dots[this.currDot.i + possibleDirection[0]][this.currDot.j + possibleDirection[1]]
+				if (nextDot.painter == null) {
+					possibleIndices.push(i);
+					if (possibleDirection[0] == this.lastDirection[0] && possibleDirection[1] == this.lastDirection[1]) {
+						possibleIndices.push(i);
+						possibleIndices.push(i);
+					}
+				}
 			}
 		}
 
-		return directionType;
+		if (possibleIndices.length > 0) {
+			return random(possibleIndices);
+		} else {
+			return -1;
+		}
 	}
 
 	move() {
 		if (this.state == 'IDLE') {
 			if (this.idleTime == 0) {
 				this.idleTime = painterIdleTime;
-
-				let directionType = this.getDirectionType();
-
 				
-				let possibleToMove = false;
-				for (let i = 0; i < painterDirections[directionType].length; i++) {
-					let possibleDirection = painterDirections[directionType][i]
-					let nextDot = dots[this.currDot.i + possibleDirection[0]][this.currDot.j + possibleDirection[1]]
-					if (nextDot.painter == null) {
-						possibleToMove = true;
-						break;
-					}
-				}
-
-				if (!possibleToMove) {
-					this.state = 'STUCK'
+				let directionIdx = this.getDirection();
+				if (directionIdx < 0) {
+					this.state = 'STUCK';
 					return;
 				}
-
-
-				let idx = floor(random(0, painterDirections[directionType].length));
-				let direction = painterDirections[directionType][idx];
-				while (dots[this.currDot.i + direction[0]][this.currDot.j + direction[1]].painter != null) {
-					idx = (idx + 1) % painterDirections[directionType].length;
-					direction = painterDirections[directionType][idx];
-				}
+				let direction = painterDirections[directionIdx];
 				let newDot = dots[this.currDot.i + direction[0]][this.currDot.j + direction[1]]
 				newDot.painter = this;
 				this.targetDot = newDot
+
+				this.lastDirection = direction;
 
 				this.state = 'MOVING'
 			} else {
@@ -172,7 +150,7 @@ function setup() {
 		dots.push(row);
 	}
 	
-	let startingDots = [dots[1][1], dots[height - 2][1], dots[height-2][width-2], dots[1][width-2]]
+	let startingDots = [dots[2][2], dots[height - 3][2], dots[height-3][width-3], dots[2][width-3]]
 
 	for (let i = 0; i < numPainters; i++) {
 		painters.push(new Painter(colorPalette[i][0], colorPalette[i][1], colorPalette[i][2],startingDots[i]))
